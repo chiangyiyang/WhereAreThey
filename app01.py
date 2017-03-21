37c3fb6f44ad8fac670d8a8a974bcbcf18a4c615
yiyang <chiangyiyang@gmail.com>
2017-03-21 10:57:50 +0800

    add "kml" api modify "list" api

--- app01.py
+++ app01.py
@@ -43,10 +43,26 @@ def listAll():
     cursor = conn.execute(sql)
     res = ""
     for row in cursor:
-        res += str(row)
+        res += str(row)+"<br>"
     conn.close()
 
-    return res
+    html="""
+            <!DOCTYPE html>
+            <html>
+            <head>
+
+            <meta http-equiv="refresh" content="5" />
+
+            <title>List of LOC</title>
+            </head>
+            <body>
+
+            <p>%s</p>
+
+            </body>
+            </html>
+            """ % res
+    return html
 
 
 @app.route("/erease_all")
@@ -58,7 +74,24 @@ def eraseAll():
     sql = "DELETE FROM LOC"
     conn.execute(sql)
     conn.commit()
+    conn.close()
     return "Done"
+
+
+@app.route("/kml")
+def getKML():
+    import sqlite3
+    import simplekml
+
+    kml = simplekml.Kml()
+    conn = sqlite3.connect('Data.db')
+    print "Opened database successfully";
+    sql = "SELECT * FROM LOC"
+    cursor = conn.execute(sql)
+    for row in cursor:
+        kml.newpoint(name=row[1], coords=[(row[2], row[3])])
+    conn.close()
+    return kml.kml()
 
 
 if __name__ == "__main__":
