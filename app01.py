07736d0176b1a5faf29478666d23d5f1f3f244dd
yiyang <chiangyiyang@gmail.com>
2017-03-21 10:04:00 +0800

    add "list" api

--- app01.py
+++ app01.py
@@ -27,13 +27,26 @@ def updateLoc(name, x, y, info):
     print "Opened database successfully";
     sql = "INSERT INTO LOC (NAME,X,Y,INFO) \
           VALUES ( '%s', %f, %f, '%s' )" % (name, float(x), float(y), info)
-    # sql = "INSERT INTO LOC (NAME, X, Y, INFO) VALUES ( '%s', %f, %f, '%s' )" % ("HH", 121.123, 23.456, "ddd")
-    print sql
     conn.execute(sql)
     conn.commit()
     conn.close()
     return "Data is inserted!!"
 
+
+@app.route("/list")
+def listAll():
+    import sqlite3
+
+    conn = sqlite3.connect('Data.db')
+    print "Opened database successfully";
+    sql = "SELECT * FROM LOC"
+    cursor = conn.execute(sql)
+    res = ""
+    for row in cursor:
+        res += str(row)
+    conn.close()
+
+    return res
 
 
 if __name__ == "__main__":
